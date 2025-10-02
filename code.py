02aa52352f5b6acf6c49b522489f8f047b8a7b4c
rubicubes <96008029+rubicubes@users.noreply.github.com>
2025-10-02 14:29:23 +0900

    Update code.py

--- code.py
+++ code.py
@@ -2,18 +2,18 @@ import re
 
 with open("wagahaihanekodearu.txt", encoding="utf-8") as f:
     text = f.read()
-
-len_four = re.findall(r"[一-龠]{4}", text)
-words = re.findall(r"[一-龠]{4}", text)
+    
+text = text.replace("\n", "")
+four = re.findall(r"[一-龠]{4}", text)
+words = re.findall(r"[一-龠]{4,}", text)
 longest_words = [""]
 length = 0
 for i in words:
   l = len(i)
   if l > length:
-    longest_word = [i]
+    longest_words = [i]
     length = l
   elif l == length:
     longest_words.append(i)
-print("四字連続漢字の数:", len(matches))
-print("最初の10個:", matches[:10])
+print(len(four))
 print(longest_words)
